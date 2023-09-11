cebc7f4a5b0652e23ee8b0ac87cea314d0b8afb1
Clibanez <clibanezzcaldas@gmail.com>
2023-09-11 16:23:43 -0300

    projeto iniciado

---
+++ src/test/java/com/clibanez/api_junit5_mockito/ApiJunit5MockitoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.clibanez.api_junit5_mockito;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiJunit5MockitoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
