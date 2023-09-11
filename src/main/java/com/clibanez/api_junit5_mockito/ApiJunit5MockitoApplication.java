650145529189686fee66c83b9e1ac7e99f079746
Clibanez <clibanezzcaldas@gmail.com>
2023-09-11 16:38:38 -0300

    classe usuario

--- src/main/java/com/clibanez/api_junit5_mockito/ApiJunit5MockitoApplication.java
+++ src/main/java/com/clibanez/api_junit5_mockito/ApiJunit5MockitoApplication.java
@@ -1,5 +1,6 @@
 package com.clibanez.api_junit5_mockito;
 
+import com.clibanez.api_junit5_mockito.domain.User;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,6 +9,7 @@ public class ApiJunit5MockitoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ApiJunit5MockitoApplication.class, args);
+		
 	}
 
 }
---
+++ src/main/java/com/clibanez/api_junit5_mockito/domain/User.java
@@ -0,0 +1,25 @@
+package com.clibanez.api_junit5_mockito.domain;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String name;
+
+    @Column(unique = true)
+    private String email;
+    private String password;
+
+
+}
